aed5934ca761f09685d9f1b7aea19046229d0509
Laura <laura.kan@hotmail.com>
2021-10-21 17:31:45 +1000

    done

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(value){
+  if (value <= 400)
+  return "This one is on me!"
+
+  if (value <2500)
+  return 'I will gladly take your thirty bucks.'
+
+  if (value > 2500)
+  return 'No can do.'
+
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(location){
+
+  if (location === 'NYC')
+  return "Ok, sounds good."
+  else 
+  return "No go."
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tips){
+  
+  if (tips === 'generous')
+  return "Thank you so much."
+  if (tips === 'not as generous')
+  return "Thank you."
+  else
+  return "Bye."
 }
